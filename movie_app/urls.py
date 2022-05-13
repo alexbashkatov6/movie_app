02ec998d2609375d0d15185bffc2e20578d10c50
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-05-13 13:51:09 +0300

    first commit

---
+++ movie_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.show_all_movie),
+    path('movie/<int:id_movie>', views.show_one_movie, name='movie-detail'),
+]
